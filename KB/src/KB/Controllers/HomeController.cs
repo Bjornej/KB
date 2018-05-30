e415fc74afb1637abbecfb3f9a873354fae854ea
Paolo Nicodemo <paolonicodemo@gmail.com>
2018-05-30 23:11:36 +0200

    added intialization views

--- KB/src/KB/Controllers/HomeController.cs
+++ KB/src/KB/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using KB.Models;
+using KB.Helpers;
 
 namespace KB.Controllers
 {
@@ -12,7 +13,43 @@ namespace KB.Controllers
     {
         public IActionResult Index()
         {
+            if (!GitHelpers.IsInitialized(Constants.RepositoryFolder))
+            {
+                return RedirectToAction("Initialize");
+            }
+
             return View();
+        }
+
+        public IActionResult Initialize()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Clone(string url)
+        {
+            if (!GitHelpers.IsInitialized(Constants.RepositoryFolder))
+            {
+                GitHelpers.Clone(Constants.RepositoryFolder, url);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Init(string url)
+        {
+            if (!GitHelpers.IsInitialized(Constants.RepositoryFolder))
+            {
+                GitHelpers.Init(Constants.RepositoryFolder);
+                if (!string.IsNullOrWhiteSpace(url))
+                {
+                    GitHelpers.AddRemote(Constants.RepositoryFolder, url);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
